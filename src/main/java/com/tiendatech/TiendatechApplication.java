78c44867e55b843901b4caf68b526f67198053a1
hugoalbertovm <hvillarreal70520@ufide.ac.cr>
2025-06-01 09:31:34 -0600

    Inicializacion del proyecto

---
+++ src/main/java/com/tiendatech/TiendatechApplication.java
@@ -0,0 +1,13 @@
+package com.tiendatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendatech/TiendatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
